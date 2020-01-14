434b605afa5df5e6a27269e28bdfcd28f755e8e7
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2020-01-13 16:17:40 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-var endpoint = https://www.jsonstore.io/0a8c5b9738244f6949fe507cc84c25c17c3c34998e02b85ae0b80c34fc04d321
+var endpoint = "https://www.jsonstore.io/0a8c5b9738244f6949fe507cc84c25c17c3c34998e02b85ae0b80c34fc04d321"
+
+function getrandom() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+function genhash(){
+    if (window.location.hash == ""){
+        window.location.hash = getrandom();
+    }
+}
+
+function shorturl(){
+    var longurl = geturl();
+    genhash();
+    send_request(longurl);
+}
+
+function send_request(url) {
+    this.url = url;
+    $.ajax({
+        'url': endpoint + "/" + window.location.hash.substr(1),
+        'type': 'POST',
+        'data': JSON.stringify(this.url),
+        'dataType': 'json',
+        'contentType': 'application/json; charset=utf-8'
+})
+}
+
+var hashh = window.location.hash.substr(1)
+
+if (window.location.hash != "") {
+    $.getJSON(endpoint + "/" + hashh, function (data) {
+        data = data["result"];
+
+        if (data != null) {
+            window.location.href = data;
+        }
+
+    });
+  
+  
